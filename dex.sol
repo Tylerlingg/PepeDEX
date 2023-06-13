04efd787ace791eca4e90daab5551591a1adcfc7
Tylerlingg <117559896+Tylerlingg@users.noreply.github.com>
2023-06-12 20:35:42 -0600

    Update dex.sol

--- dex.sol
+++ dex.sol
@@ -14,13 +14,14 @@ contract SimpleTokenSwap is Ownable, Pausable {
     uint256 public minimumSwapAmount;
     uint256 public maximumSwapAmount;
 
-    event SwapRateChanged(uint256 oldRate, uint256 newRate);
+    event SwapRateChanged(address indexed changer, uint256 oldRate, uint256 newRate);
+    event SwapLimitsChanged(address indexed changer, uint256 oldMinAmount, uint256 oldMaxAmount, uint256 newMinAmount, uint256 newMaxAmount);
     event Swapped(address indexed user, uint256 amountIn, uint256 amountOut);
     event TokensWithdrawn(address account, uint256 amount, uint256 remainingBalance);
 
     constructor(IERC20 _token, uint256 _swapRate, uint256 _minimumSwapAmount, uint256 _maximumSwapAmount) {
         require(_swapRate > 0, "Swap rate must be greater than 0");
-        require(_minimumSwapAmount < _maximumSwapAmount, "Minimum must be less than maximum");
+        require(_minimumSwapAmount < _maximumSwapAmount, "Minimum swap amount must be less than maximum swap amount");
 
         token = _token;
         swapRate = _swapRate;
@@ -29,14 +30,14 @@ contract SimpleTokenSwap is Ownable, Pausable {
     }
 
     function swap(uint256 amount) external whenNotPaused {
-        require(amount >= minimumSwapAmount, "Amount too low");
-        require(amount <= maximumSwapAmount, "Amount too high");
+        require(amount >= minimumSwapAmount, "Swap amount too low");
+        require(amount <= maximumSwapAmount, "Swap amount too high");
 
         uint256 amountToSendBack = calculateSwap(amount);
-        require(token.balanceOf(address(this)) >= amountToSendBack, "Not enough tokens in contract");
+        require(token.balanceOf(address(this)) >= amountToSendBack, "Not enough tokens in the contract for swap");
 
-        require(token.transferFrom(msg.sender, address(this), amount), "Transfer from sender failed");
-        require(token.transfer(msg.sender, amountToSendBack), "Transfer to sender failed");
+        require(token.transferFrom(msg.sender, address(this), amount), "Failed to transfer tokens from sender to contract");
+        require(token.transfer(msg.sender, amountToSendBack), "Failed to transfer tokens from contract to sender");
 
         emit Swapped(msg.sender, amount, amountToSendBack);
     }
@@ -50,20 +51,25 @@ contract SimpleTokenSwap is Ownable, Pausable {
         uint256 oldRate = swapRate;
         swapRate = _swapRate;
 
-        emit SwapRateChanged(oldRate, _swapRate);
+        emit SwapRateChanged(msg.sender, oldRate, _swapRate);
     }
 
     function setSwapLimits(uint256 _minimumSwapAmount, uint256 _maximumSwapAmount) external onlyOwner {
-        require(_minimumSwapAmount < _maximumSwapAmount, "Minimum must be less than maximum");
+        require(_minimumSwapAmount < _maximumSwapAmount, "Minimum swap amount must be less than maximum swap amount");
 
+        uint256 oldMinAmount = minimumSwapAmount;
+        uint256 oldMaxAmount = maximumSwapAmount;
+        
         minimumSwapAmount = _minimumSwapAmount;
         maximumSwapAmount = _maximumSwapAmount;
+
+        emit SwapLimitsChanged(msg.sender, oldMinAmount, oldMaxAmount, _minimumSwapAmount, _maximumSwapAmount);
     }
 
     function withdrawTokens(uint256 amount) external onlyOwner whenNotPaused {
-        require(token.balanceOf(address(this)) >= amount, "Not enough tokens");
+        require(token.balanceOf(address(this)) >= amount, "Not enough tokens in the contract to withdraw");
 
-        require(token.transfer(owner(), amount), "Transfer failed");
+        require(token.transfer(owner(), amount), "Failed to transfer tokens from contract to owner");
         uint256 remainingBalance = token.balanceOf(address(this));
         
         emit TokensWithdrawn(msg.sender, amount, remainingBalance);
