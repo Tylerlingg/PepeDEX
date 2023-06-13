50a9af5362609d0fc6d04a033201e5ed5b6e25f0
Ling <117559896+Tylerlingg@users.noreply.github.com>
2023-06-13 11:34:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
-// Import your contract's ABI and address
-const contractAbi = [...];
-const contractAddress = "...";
-
-// Initialize ethers.js
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
-const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+const contractAbi = [
+    // Replace this array with your contract's actual ABI
+];
+const contractAddress = "0xYourContractAddress"; // Replace this with your contract's actual address
+
+const contract = new ethers.Contract(contractAddress, contractAbi, provider).connect(signer);
 
 // Set up form event listener for swap
 document.getElementById("swap-form").addEventListener("submit", async function(event) {
     event.preventDefault();
 
-    const amount = ethers.utils.parseEther(document.getElementById("amount").value);
-    const slippage = document.getElementById("slippage").value;
-    const deadline = document.getElementById("deadline").value;
+    const amountIn = ethers.utils.parseEther(document.getElementById("amount-in").value);
+    const maxSlippagePercentage = parseInt(document.getElementById("max-slippage-percentage").value);
+    const deadline = parseInt(document.getElementById("deadline").value);
 
     try {
-        const tx = await contract.swap(amount, slippage, deadline, { value: amount });
+        const tx = await contract.swap(amountIn, maxSlippagePercentage, deadline, { value: amountIn });
         alert(`Transaction sent with hash: ${tx.hash}`);
     } catch (error) {
         alert(`Error: ${error.message}`);
     }
 });
 
-// Set up form event listener for add liquidity
+// Set up form event listener for addLiquidity
 document.getElementById("add-liquidity-form").addEventListener("submit", async function(event) {
     event.preventDefault();
 
-    const amount = ethers.utils.parseEther(document.getElementById("add-liquidity-amount").value);
-    
+    const amountETH = ethers.utils.parseEther(document.getElementById("amount-eth").value);
+    const amountToken = ethers.utils.parseEther(document.getElementById("amount-token").value);
+
     try {
-        const tx = await contract.addLiquidity(amount, { value: amount });
-        alert(`Transaction sent with hash: ${tx.hash}`);
+        // Approve the contract to spend tokens on behalf of the user
+        const approveTx = await contract.approveToken(amountToken);
+        await approveTx.wait();
+
+        // Add liquidity
+        const addLiquidityTx = await contract.addLiquidity(amountToken, { value: amountETH });
+        alert(`Transaction sent with hash: ${addLiquidityTx.hash}`);
     } catch (error) {
         alert(`Error: ${error.message}`);
     }
 });
 
-//Set up form event listener for remove liquidity
+// Set up form event listener for removeLiquidity
 document.getElementById("remove-liquidity-form").addEventListener("submit", async function(event) {
     event.preventDefault();
 
