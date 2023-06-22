7ecc39933f9985a3b3253398592d5283986e203e
Ling <117559896+Tylerlingg@users.noreply.github.com>
2023-06-21 22:16:37 -0600

    Update pepeDexV1.sol

--- pepeDexV1.sol
+++ pepeDexV1.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+// Importing required interfaces and security features from OpenZeppelin library
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+// Interface for interacting with Uniswap V3 pool
 interface IUniswapV3Pool {
     function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol);
 }
 
-/**
- * @title pepeDex
- * @dev A simple automated market maker (AMM) that allows users to swap tokens, add liquidity, and remove liquidity.
- * Liquidity providers can also claim their share of accumulated fees.
- */
+// Defining the pepeDex contract
 contract pepeDex is ReentrancyGuard {
+    // State variables
     IERC20 public immutable token;
     mapping(address => uint256) public liquidityBalance;
     uint256 public totalLiquidity;
@@ -27,149 +26,137 @@ contract pepeDex is ReentrancyGuard {
     Reserves public reserves;
 
     uint256 public constant FEE_PERCENTAGE = 3; // Represents 0.3%
+    uint256 public constant TWAP_PERIOD = 5 minutes; // TWAP period set to 5 minutes
+    uint256 public constant MINIMUM_TIME_BETWEEN_SWAPS = 3 minutes; // Minimum time between swaps by a user
 
     address public uniswapPool;
 
+    // Variables for TWAP calculation
+    uint256 public lastPrice;
+    uint256 public lastPriceUpdateTime;
+
+    // Tracks the last swap time of each user
+    mapping(address => uint256) public lastSwapTime;
+
+    // Events
     event Swapped(address indexed user, uint256 amountIn, uint256 amountOut);
     event LiquidityAdded(address indexed user, uint256 amountETH, uint256 amountToken);
     event LiquidityRemoved(address indexed user, uint256 amountETH, uint256 amountToken);
     event FeesClaimed(address indexed user, uint256 amount);
 
-    /**
-     * @dev Constructor sets the token and Uniswap pool.
-     * @param _token The address of the ERC20 token.
-     * @param _uniswapPool The address of the Uniswap V3 pool for Pepe/ETH.
-     */
+    // Constructor initializes the contract
     constructor(IERC20 _token, address _uniswapPool) {
         token = _token;
         uniswapPool = _uniswapPool;
     }
 
-    /**
-     * @dev Retrieve the latest price from Uniswap V3.
-     * @return The latest price.
-     */
+    // Fetches the latest price from Uniswap
     function getLatestPrice() public view returns (uint256) {
         IUniswapV3Pool pool = IUniswapV3Pool(uniswapPool);
         (uint160 sqrtPriceX96, , , , , ) = pool.slot0();
-        // The price is represented as the square root of the ratio between token amounts,
-        // adjusted by a factor of 2^96. To get the price, square the value and scale it down.
         uint256 price = uint256(sqrtPriceX96);
         price = price * price * 1e18 / (1 << 192) / (1 << 192);
         return price;
     }
 
-    /**
-     * @dev Adds liquidity to the exchange.
-     * @param amountToken Amount of tokens to add as liquidity.
-     */
-    function addLiquidity(uint256 amountToken) external payable nonReentrant {
-        uint256 ethSent = msg.value;
-        require(amountToken > 0 && ethSent > 0, "Cannot add zero liquidity");
-
-        uint256 liquidity = ethSent;
-        if (totalLiquidity != 0) {
-            uint256 latestPrice = getLatestPrice();
-            liquidity = latestPrice * amountToken * 1e18 / reserves.reserveToken;
+    // Calculates the Time-Weighted Average Price (TWAP)
+    function getTwapPrice() public view returns (uint256) {
+        uint256 currentTime = block.timestamp;
+        uint256 timeElapsed = currentTime - lastPriceUpdateTime;
+        uint256 currentPrice = getLatestPrice();
+        if (timeElapsed >TWAP_PERIOD) {
+            timeElapsed = TWAP_PERIOD;
         }
+        return (lastPrice * (TWAP_PERIOD - timeElapsed) + currentPrice * timeElapsed) / TWAP_PERIOD;
+    }
 
-        totalLiquidity += liquidity;
-        liquidityBalance[msg.sender] += liquidity;
-        reserves.reserveETH += ethSent;
-       reserves.reserveToken += amountToken;
-        lastAccumulatedFees[msg.sender] = accumulatedFees * liquidity / totalLiquidity;
+    // Internal function to update the TWAP
+    function updateTwap() internal {
+        lastPrice = getLatestPrice();
+        lastPriceUpdateTime = block.timestamp;
+    }
+
+    // Allows users to add liquidity to the pool
+    function addLiquidity(uint256 amountToken) external payable nonReentrant {
+        uint256 ethAmount = msg.value;
+        uint256 liquidityAmount = (ethAmount * totalLiquidity) / reserves.reserveETH + 1;
+
+        liquidityBalance[msg.sender] += liquidityAmount;
+        totalLiquidity += liquidityAmount;
+        reserves.reserveETH += ethAmount;
+        reserves.reserveToken += amountToken;
 
         token.transferFrom(msg.sender, address(this), amountToken);
 
-        emit LiquidityAdded(msg.sender, ethSent, amountToken);
+        // Update TWAP after liquidity is added
+        updateTwap();
+
+        emit LiquidityAdded(msg.sender, ethAmount, amountToken);
     }
 
-    /**
-     * @dev Removes liquidity from the exchange.
-     * @param amountLiquidity Amount of liquidity to remove.
-     */
-    function removeLiquidity(uint256 amountLiquidity) external nonReentrant {
-        require(amountLiquidity > 0 && liquidityBalance[msg.sender] >= amountLiquidity, "Invalid liquidity amount");
+    // Allows users to swap ETH for tokens
+    function swap(uint256 amountIn, uint256 amountOutMin, uint8 maxSlippage, uint256 maxTwapDeviation, uint256 deadline) external payable nonReentrant {
+        // Ensure that the transaction is not expired
+        require(block.timestamp <= deadline, "Transaction expired");
 
-        uint256 ethAmount = reserves.reserveETH * amountLiquidity / totalLiquidity;
-        uint256 tokenAmount = reserves.reserveToken * amountLiquidity / totalLiquidity;
+        // Rate limiting: Ensure that the user cannot swap too frequently
+        require(block.timestamp >= lastSwapTime[msg.sender] + MINIMUM_TIME_BETWEEN_SWAPS, "Swap too soon");
+
+        uint256 amountOut = getAmountOut(amountIn);
+        require(amountOut >= amountOutMin, "Slippage tolerance exceeded");
+
+        reserves.reserveToken -= amountOut;
+        reserves.reserveETH += amountIn;
+
+        // Update the last swap time for the user
+        lastSwapTime[msg.sender] = block.timestamp;
+
+        // Update TWAP after swap
+        updateTwap();
+
+        emit Swapped(msg.sender, amountIn, amountOut);
+    }
+
+    // Calculates the amount of tokens a user receives for a given amount of ETH
+    function getAmountOut(uint256 amountIn) internal view returns (uint256) {
+        uint256 amountInWithFee = amountIn * (1000 - FEE_PERCENTAGE);
+        uint256 numerator = amountInWithFee * reserves.reserveETH;
+        uint256 denominator = reserves.reserveToken * 1000 + amountInWithFee;
+        return numerator / denominator;
+    }
+
+    // Allows users to remove liquidity
+    function removeLiquidity(uint256 liquidityAmount) external nonReentrant {
+        require(liquidityBalance[msg.sender] >= liquidityAmount, "Not enough liquidity");
+
+        uint256 ethAmount = (reserves.reserveETH * liquidityAmount) / totalLiquidity;
+        uint256 tokenAmount = (reserves.reserveToken * liquidityAmount) / totalLiquidity;
 
         reserves.reserveETH -= ethAmount;
         reserves.reserveToken -= tokenAmount;
-        liquidityBalance[msg.sender] -= amountLiquidity;
-        totalLiquidity -= amountLiquidity;
+        liquidityBalance[msg.sender] -= liquidityAmount;
+        totalLiquidity -= liquidityAmount;
 
-        (bool ethSuccess,) = msg.sender.call{value: ethAmount}("");
-        require(ethSuccess, "ETH transfer failed");
+        payable(msg.sender).transfer(ethAmount);
         token.transfer(msg.sender, tokenAmount);
+
+        // Update TWAP after liquidity is removed
+        updateTwap();
 
         emit LiquidityRemoved(msg.sender, ethAmount, tokenAmount);
     }
 
-    /**
-     * @dev Swaps ETH for tokens.
-     * @param amountOutMin Minimum amount of tokens expected to receive.
-     * @param maxSlippage Maximum acceptable slippage in percentage points (e.g., 5 for 5%).
-     */
-    function swap(uint256 amountOutMin, uint256 maxSlippage) external payable nonReentrant {
-        uint256 ethIn = msg.value;
-        require(ethIn > 0, "Cannot swap zero ETH");
-
-        uint256 amountOut = getAmountOut(ethIn);
-        uint256 slippage = amountOut * maxSlippage / 1000; // maxSlippage is in percentage points scaled by 10 (e.g., 5 for 0.5%)
-        uint256 amountOutWithSlippage = amountOut - slippage;
-        require(amountOutWithSlippage >= amountOutMin, "Slippage too high");
-
-        reserves.reserveETH += ethIn;
-        reserves.reserveToken -= amountOut;
-        accumulatedFees += ethIn * FEE_PERCENTAGE / 1000;
-
-        token.transfer(msg.sender, amountOutWithSlippage);
-
-        emit Swapped(msg.sender, ethIn, amountOutWithSlippage);
-    }
-
-    /**
-     * @dev Claims the accumulated fees.
-     */
+    // Allows liquidity providers to claim fees
     function claimFees() external nonReentrant {
-        uint256 userLiquidity = liquidityBalance[msg.sender];
-        require(userLiquidity > 0, "No liquidity provided");
+        uint256 fees = (accumulatedFees * liquidityBalance[msg.sender]) / totalLiquidity - lastAccumulatedFees[msg.sender];
+        require(fees > 0, "No fees to claim");
 
-        uint256 claimableFees = accumulatedFees * userLiquidity / totalLiquidity - lastAccumulatedFees[msg.sender];
+        lastAccumulatedFees[msg.sender] += fees;
+        token.transfer(msg.sender, fees);
 
-        lastAccumulatedFees[msg.sender] = accumulatedFees * userLiquidity / totalLiquidity;
-
-        token.transfer(msg.sender, claimableFees);
-
-        emit FeesClaimed(msg.sender, claimableFees);
+        emit FeesClaimed(msg.sender, fees);
     }
 
-    /**
-     * @dev Calculates the amount of tokens that can be bought with a given amount of ETH.
-     * @param amountIn Amount of ETH.
-     * @return Amount of tokens.
-     */
-    function getAmountOut(uint256 amountIn) public view returns (uint256) {
-        uint256 amountInWithFee = amountIn * (1000 - FEE_PERCENTAGE);
-        uint256 numerator = amountInWithFee * reserves.reserveToken;
-        uint256 denominator = reserves.reserveETH * 1000 + amountInWithFee;
-        return numerator / denominator;
-    }
+    // . . . end . . .
 
-    /**
-     * @dev Reverts the transaction if the call is not successful.
-     * Used as a low level call to implement transfer of ETH.
-     * @param to Address to transfer ETH to.
-     * @param value Amount of ETH to transfer.
-     */
-    function safeTransferETH(address to, uint256 value) internal {
-        (bool success,) = to.call{value: value}("");
-        require(success, "ETH transfer failed");
-    }
-
-    /**
-     * @dev Function to allow contract to receive ETH.
-     */
-    receive() external payable {}
 }
